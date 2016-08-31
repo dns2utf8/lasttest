a1a5f71a74cc0c714de488e1d96bb77da63b3561
Stefan Schindler <stefan@estada.ch>
2016-08-31 12:28:59 +0200

    Improove code with rustup run nightly cargo clippy --release

--- src/net.rs
+++ src/net.rs
@@ -12,15 +12,15 @@ pub fn main_net(args : &::Args, pool : &ThreadPool) {
 fn run_client(args : &::Args, pool : &ThreadPool) {
   println!("client: {:?}", args);
   let port = get_default_port(&args);
-  let ref servername = args.arg_servername;
+  let servername = &args.arg_servername;
 }
 
 fn run_server(args : &::Args, pool : &ThreadPool) {
-  let port = get_default_port(&args);
+  let port = get_default_port(args);
   println!("server mode: port({})", port);
 }
 
 
 fn get_default_port(args : &::Args) -> u16 {
-  if args.arg_port == 0 { 5278 } else { args.arg_port.clone() }
+  if args.arg_port == 0 { 5278 } else { args.arg_port }
 }
